7c5024fac8fb09e1282c3129e7b0251325413b13
foxymplayer <gufoxy194@gmail.com>
2024-09-11 10:15:35 -0300

    adicionei maneiras de validar o login

---
+++ controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+//requer arquivo 'user.php' que contem o model user com as funções para manipulação de dados de usuarios//
+require_once 'models/user.php';
+
+class AuthController{
+    //cria função responsavel pelo processo de login//
+    public function login(){
+        
+    }
+}
+?>
---
+++ routes.php
@@ -0,0 +1,19 @@
+<?php
+
+// inclui arquivos de controlador para lidar com diferentes ações//
+require 'controllers/AuthController.php';
+require 'controllers/UserController.php';
+require 'controllers/DashboardController.php';
+
+//cria instancias dos controladores para ultilizar seus metodos//
+$authController = new AuthController();
+
+//coleta a ação da url, se não tiver nenhum login definido ele usa 'login' por padão//
+$action = $_GET['action'] ?? 'login';
+
+// verifica a ação solicitada e chama o metodo apropriado do controlador//
+switch($action){
+    case 'login':
+        $authController->login();
+}
+?>
