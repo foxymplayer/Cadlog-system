6a9e8e272a735dc4d4f9ac0fdc15a18046636085
foxymplayer <gufoxy194@gmail.com>
2024-09-17 12:08:57 -0300

    adicionei as funcionalidades de registro

---
+++ views/register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar-se</title>
+</head>
+<body>
+    <div>
+        <h2>Cadastro de usuario</h2>
+        <form action="" method="post">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="nome" require>
+
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" require>
+
+            <label for="senha">Senha</label>
+            <input type="password" name="senha" id="senha" require>
+
+            <label for="perfil">Perfil:</label>
+            <select name="perfil" id="perfil">
+                <option value="gdmin">Admin</option>
+                <option value="gestor">Gestor</option>
+                <option value="colaborador">Colaborador</option>
+
+                <button type="submit">Cadastrar</button>
+            </select>
+        </form>
+
+        <a href="">Voltar ao Login</a>
+    </div>
+</body>
+</html>
